cefc95fda7192f09291d44cb089c3cc5e89b8e7f
Christopher J. Seeger <cjseeger@iastate.edu>
2019-10-11 21:31:42 -0500

    Update meetsHeld.php

--- meetsHeld.php
+++ meetsHeld.php
@@ -18,14 +18,12 @@ $table = 'XC_meetDates';
 
 
 $dailyMeets=array();
+$arrayPosition = 1;
 
 
-$arrayPosition = 0;
-
-
-for ($startingWeek  = 41; $startingWeek < 42; $startingWeek++) {
+for ($startingWeek  = 35; $startingWeek < 45; $startingWeek++) {
 	for ($startingDay  = 1; $startingDay  <= 7; $startingDay++) {
-		$sql = "SELECT *, COUNT(meet) AS meets FROM $table GROUP BY date WHERE week = $startingWeek AND day = $startingDay";
+		$sql = "SELECT *, COUNT(meet) AS meets FROM $table WHERE week = $startingWeek AND day = $startingDay";
 		//print $sql."<br>";
 
 
@@ -39,19 +37,81 @@ for ($startingWeek  = 41; $startingWeek < 42; $startingWeek++) {
 			    $week = $row["week"];
 			    $day = $row["day"];
 			     //print $name."<br>";
-			     print $date." ".$meets."<br>";
+			    //print $date." ".$meets."<br>";
 			     $dailyMeets[$arrayPosition] = $meets;
 		    }
 		}else{
 			$dailyMeets[$arrayPosition] = 0;
 		}
-		print $dailyMeets[$arrayPosition]."<br>";
+		//print $arrayPosition.". ".$startingWeek." ".$startingDay." - ".$dailyMeets[$arrayPosition]."<br>";
 		$arrayPosition++;
 	}
 };
 
 
 
+//count the status
+
+$dailyMeetsCancelled=array();
+$arrayPosition = 1;
+for ($startingWeek  = 35; $startingWeek < 45; $startingWeek++) {
+	for ($startingDay  = 1; $startingDay  <= 7; $startingDay++) {
+		$sql = "SELECT *, COUNT(meet) AS meets FROM $table WHERE week = $startingWeek AND day = $startingDay AND status = 'cancelled' ";
+		//print $sql."<br>";
+
+
+		$result = $mysqli->query($sql);
+		if ($result->num_rows > 0) {
+		    // output data of each row
+		    while($row = $result->fetch_assoc()) {
+			    $name = $row["year"];
+			    $date = $row["date"];
+			    $meets = $row["meets"];
+			    $week = $row["week"];
+			    $day = $row["day"];
+			     //print $name."<br>";
+			    //print $date." ".$meets."<br>";
+			     $dailyMeetsCancelled[$arrayPosition] = $meets;
+		    }
+		}else{
+			$dailyMeetsCancelled[$arrayPosition] = 0;
+		}
+		//print $arrayPosition.". ".$startingWeek." ".$startingDay." - ".$dailyMeetsCancelled[$arrayPosition]."<br>";
+		$arrayPosition++;
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//count($dailyMeets)
+
+//print $dailyMeets[8];
+$table = "";
+$v=0;
+
+for ($j  = 1; $j < 11; $j++) {
+	$table .= "<tr height='70px'>";
+	for ($i  = 1; $i <= 7; $i++) {
+		$v++;
+		$p = round((($dailyMeets[$v]-$dailyMeetsCancelled[$v])/$dailyMeets[$v])*100)."%";
+		if ($p == "0%") {
+			$p = "&nbsp;";
+		}
+		$table .= "<td>".$p."</td>";
+	}
+	$table .= "</tr>";
+}
 
 
 
@@ -76,21 +136,22 @@ html, body {
 }
 
 #meetCalGrid{
-	width: 500px;
-	font-size: small;
+	width: 490px;
+	font-size: xx-small;
 	font-family: sans-serif;
 	border-color: #000;
 	border-style: solid;
 	border-width: 0px;
-	 padding: 5px;
-	  border-collapse: collapse;
+	padding: 0px;
+	border-collapse: collapse;
 }
 
 
 
 td {
-  border-bottom: 1px solid #000;
+  border: 1px solid #000;
   text-align: center;
+  box-sizing: border-box;
 }
 
 th {
@@ -109,23 +170,15 @@ th {
 <body>
 <table id='meetCalGrid'>
 	<tr>
-		<th>Sunday</th>
-		<th>Monday</th>
-		<th>Tuesday</th>
-		<th>Wednesday</th>
-		<th>Thursday</th>
-		<th>Friday</th>
-		<th>Saturday</th>
+		<th width=70px; >Monday</th>
+		<th width=70px; >Tuesday</th>
+		<th width=70px; >Wednesday</th>
+		<th width=70px; >Thursday</th>
+		<th width=70px; >Friday</th>
+		<th width=70px; >Saturday</th>
+		<th width=70px; >Sunday</th>
 	</tr>
-	<tr>
-		<td>10%</td>
-		<td>10%</td>
-		<td>10%</td>
-		<td>10%</td>
-		<td>10%</td>
-		<td>10%</td>
-		<td>10%</td>
-	</tr>
+	<?php print $table; ?>
 
 </table>
 
